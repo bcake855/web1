659c789f36d09c1f668f4a07b070fb2cf27fb3dd
bcake855 <bcake855@maxschwsmacbook.fritz.box>
2018-01-20 15:51:46 +0100

    added web.xml + servlet

---
+++ Web1/src/servlets/HelloServlet.java
@@ -0,0 +1,34 @@
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+@WebServlet("/HelloServlet")
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter writer = response.getWriter();
+		writer.println("<html>");
+		writer.println("<body>");
+		writer.println("Hello from servlet!");
+		writer.println("</body>");
+		writer.println("</html>");
+	}
+
+
+}
