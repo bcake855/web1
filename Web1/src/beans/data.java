571f08831f6b97c69bb56239f4ef2aa4a5466b48
bcake855 <bcake855@maxschwsmacbook.fritz.box>
2018-01-20 13:38:49 +0100

    first try to commit

---
+++ Web1/src/beans/data.java
@@ -0,0 +1,9 @@
+package beans;
+
+public class data {
+	
+	public void printMax(){
+		System.out.print("From Class Print Mad Max");
+	}
+
+}
---
+++ Web1/src/beans/start.java
@@ -0,0 +1,15 @@
+package beans;
+
+public class start {
+
+	public static void main(String[] args) {
+	System.out.println("Hello MadMax");
+	System.out.println("!");
+	System.out.println("Hello MadMax");
+	
+	data st= new data();
+	st.printMax();
+	
+	}
+
+}
