5c19ef2b29fa4db255cad99a08d536f2555634cc
max <max@thinkmax.fritz.box>
2019-06-26 22:06:00 +0200

    read all cells and rows

--- Playground/src/ExcelHandler.java
+++ Playground/src/ExcelHandler.java
@@ -1,12 +1,7 @@
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
-import javax.swing.text.html.HTMLDocument.HTMLReader.SpecialAction;
-
-import org.apache.poi.*;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.poifs.filesystem.POIFSFileSystem;
@@ -15,7 +10,7 @@ import org.apache.poi.ss.usermodel.Row;
 public class ExcelHandler {
 
 	private static final String mockUp1 = "/home/max/Desktop/mockuptest1.xls";
-	private static final String mockUp2 = "/home/max/Desktop/mockuptest2.csv";
+	private static final String mockUp2 = "/home/max/Desktop/mockuptest2.xls";
 	
 	public ExcelHandler() {
 		
@@ -26,27 +21,53 @@ public class ExcelHandler {
 		
 		try {
 			POIFSFileSystem f1 = new POIFSFileSystem(new File(mockUp1));
-			HSSFWorkbook wb = new HSSFWorkbook(f1);
+			POIFSFileSystem f2 = new POIFSFileSystem(new File(mockUp2));
 			
-			HSSFSheet spreadSheet1 = wb.getSheetAt(0);
-			HSSFSheet spreadSheet2 = wb.getSheetAt(0);
+			HSSFWorkbook wb1 = new HSSFWorkbook(f1);
+			HSSFWorkbook wb2 = new HSSFWorkbook(f1);
+			
+			HSSFSheet spreadSheet1 = wb1.getSheetAt(0);
+			HSSFSheet spreadSheet2 = wb2.getSheetAt(0);
 			
 			Row row1 = spreadSheet1.getRow(0);
 			Row row2 = spreadSheet2.getRow(0);
 			
+			int usedRowsMockUp1 = spreadSheet1.getLastRowNum();
+			int usedRowsMockUp2 = spreadSheet2.getLastRowNum();
 			
+			int usedCellsMockUp1 = row1.getLastCellNum();
+			int usedCellsMockUp2 = row2.getLastCellNum();
 			
-			for(int i=1; i < row1.getLastCellNum(); i++) {
-				System.out.println(row1.getCell(i)); 
+			ArrayList<String> rowArr1 = new ArrayList<String>();
+			ArrayList<String> rowArr2 = new ArrayList<String>();
+			
+			//if rows and columns equal
+			if(usedRowsMockUp1 == usedRowsMockUp2 && usedCellsMockUp1 == usedCellsMockUp2) {
 				
-				//for(int k=1; k < row2.getLastCellNum(); k++) {
-					//if at least one match than print
-					// if(!row1.getCell(i).equals(row2.getCell(k))) {
-						// System.out.println(row2.getCell(k));
-					 //}
-				//}
+				for(int a = 1; a <= usedRowsMockUp1; a++) {
+					//start with first data column
+					row1 = spreadSheet1.getRow(a);
+					row2 = spreadSheet2.getRow(a);
+					//write into arrays
+					for(int i=1; i <= usedCellsMockUp1; i++) {
+						String value = (row1.getCell(i) != null ? row1.getCell(i).toString() : "");
+						rowArr1.add( value != null ? value : "" );
+					}
+					
+					//for(int k=1; k < usedCellsMockUp1; k++) {
+					//	rowArr2.add(row2.getCell(k).toString());
+					//}
+					
+					for (String string : rowArr1) {
+						System.out.print(string);
+					}
+					System.out.println("");
+					
+					rowArr1.clear();
+				}
 				
 			}
+			
 			
 			
 		} catch (IOException e) {
